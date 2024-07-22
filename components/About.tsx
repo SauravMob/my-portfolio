c6ba48db11812f193980123f7146cb4ac00b8e4f
Saurav193 <saurav.upadhyay@mobavenue.com>
2024-07-22 17:38:03 +0530

    About animation added

--- components/About.tsx
+++ components/About.tsx
@@ -10,8 +10,32 @@ const About = () => {
             className='w-screen h-screen sm:min-h-[90vh] bg-gradient-to-r to-neutral-950 from-neutral-900 p-16 cursor-default grid grid-cols-2 gap-4'
         >
             <div className='col-span-1 bg-neutral-800 rounded-xl p-8'>
-                <div className='flex justify-center text-4xl font-bold text-gray-300 border-b pb-2'>About</div>
-                <div className='col-span-2 p-8 text-xl my-auto text-gray-300'>
+                <motion.div
+                    initial={{ x: -150 }}
+                    whileInView={{ x: 0 }}
+                    exit={{ x: -150 }}
+                    transition={{
+                        type: "spring",
+                        stiffness: 260,
+                        damping: 20,
+                        duration: 1.5,
+
+                    }}
+                    className='flex justify-center text-4xl font-bold text-gray-300 border-b pb-2'
+                >About</motion.div>
+                <motion.div
+                    initial={{ x: 150 }}
+                    whileInView={{ x: 0 }}
+                    exit={{ x: 150 }}
+                    transition={{
+                        type: "spring",
+                        stiffness: 260,
+                        damping: 20,
+                        duration: 1.5,
+
+                    }}
+                    className='col-span-2 p-8 text-xl my-auto text-gray-300'
+                >
                     <div>
                         Hello! I'm delighted to introduce myself.
                     </div>
@@ -21,15 +45,27 @@ const About = () => {
                     <div>
                         Over the past two years, I've had the opportunity to work with a variety of web technologies, which has significantly enriched my expertise. My curiosity drives me to continuously learn and implement new technologies, leading to the creation of impactful and innovative projects.
                     </div>
-                </div>
+                </motion.div>
             </div>
-            <div className='flex justify-center items-center'>
+            <motion.div
+                initial={{ opacity: 0.5, scale: 0.5 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.5 }}
+                transition={{
+                    type: "spring",
+                    stiffness: 260,
+                    damping: 20,
+                    duration: 0.5,
+
+                }}
+                className='flex justify-center items-center'
+            >
                 <Image
                     src={real_profile_pic}
                     className='rounded-lg'
                     alt="Back Image"
                 />
-            </div>
+            </motion.div>
         </div>
     )
 }
