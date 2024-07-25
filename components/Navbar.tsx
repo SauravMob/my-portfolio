9710053dd24721b4e13c3e9eee23e09d3ffc7f8a
Saurav193 <saurav.upadhyay@mobavenue.com>
2024-07-25 16:57:04 +0530

    Minor navbar changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,11 +22,15 @@ const Navbar = () => {
                     className='m-4 cursor-pointer absolute'
                     onClick={() => setShow(false)}
                 />}
-                {show && <div className='flex flex-col justify-around items-center bg-neutral-900 font-bold text-xl h-full w-full'>
-                    <Link href='/#home' className='text-gray-50 min-w-64 border p-4 rounded-xl text-center' onClick={() => setShow(false)}>Home</Link>
-                    <Link href='/#about' className='text-gray-50 min-w-64 border p-4 rounded-xl text-center' onClick={() => setShow(false)}>About</Link>
-                    <Link href='/#skills' className='text-gray-50 min-w-64 border p-4 rounded-xl text-center' onClick={() => setShow(false)}>Skills & Experience</Link>
-                    <Link href='/#projects' className='text-gray-50 min-w-64 border p-4 rounded-xl text-center' onClick={() => setShow(false)}>Projects</Link>
+                {show && <div className='flex flex-col mt-32 items-center bg-neutral-900 font-bold text-xl h-full w-full'>
+                    <Link href='/#home' className='text-gray-50 min-w-64 rounded-xl text-center my-4' onClick={() => setShow(false)}>Home</Link>
+                    <Link href='/#about' className='text-gray-50 min-w-64 rounded-xl text-center my-4' onClick={() => setShow(false)}>About</Link>
+                    <Link href='/#skills' className='text-gray-50 min-w-64 rounded-xl text-center my-4' onClick={() => setShow(false)}>Skills & Experience</Link>
+                    <Link href='/#projects' className='text-gray-50 min-w-64 rounded-xl text-center my-4' onClick={() => setShow(false)}>Projects</Link>
+                    <div className='mt-20 text-white text-sm flex flex-col items-center gap-2'>
+                        <div>Designed by Saurav Upadhyay</div>
+                        <div className='flex gap-1 items-center text-[12px] font-semibold cursor-pointer'><Tablet size={18} /> +91 7021496525</div>
+                    </div>
                 </div>}
             </div>
             <div className='hidden lg:flex fixed w-full justify-between text-gray-200 text-[16px] bg-transparent p-4 z-10'>
